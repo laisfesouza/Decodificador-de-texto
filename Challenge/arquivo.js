d3ad291c169902d515feef8c4dfbca4bbc907617
Lais Souza <69878868+laisfesouza@users.noreply.github.com>
2022-09-01 23:42:14 -0300

    Add files via upload

---
+++ Challenge/arquivo.js
@@ -0,0 +1,110 @@
+var botaoCriptografar = document.querySelector(".btn-criptografar");
+var botaoDescriptografar = document.querySelector (".btn-descriptografar");
+var imagem1 = document.querySelector (".imagem1");
+var h3 = document.querySelector (".titulo-h3");
+var paragrafo = document.querySelector (".paragrafo");
+var resultado = document.querySelector(".texto-resultado");
+
+botaoCriptografar.onclick = criptografar;
+botaoDescriptografar.onclick = descriptografar;
+botaoCopiar.onclick = copiar;
+
+function criptografar() {
+    ocultar();
+    var area = recuperarTexto();
+    resultado.textContent = criptografarTexto(area);
+}
+
+function descriptografar (){
+    ocultar();
+    var area = recuperarTexto();
+    resultado.textContent = descriptografarTexto(area);
+}
+
+function recuperarTexto() {
+    var area = document.querySelector(".area");
+    return area.value;
+}
+
+function ocultar (){
+    imagem1.classList.add("ocultar");
+    h3.classList.add("ocultar");
+    paragrafo.classList.add("ocultar")
+}
+
+function criptografarTexto(mensagem) {
+    var texto = mensagem;
+    var textoFinal = "";
+
+    for(var i = 0; i < texto.length; i++) {
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "ai"
+        }
+
+         else if(texto[i] == "e"){
+            textoFinal = textoFinal + "enter"
+        }
+
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "imes"
+        }
+
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "ober"
+        }
+
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "ufat"
+        }
+
+        else {
+            textoFinal = textoFinal + texto[i];
+        }
+    }
+
+   return textoFinal;
+}
+
+function descriptografarTexto(mensagem) {
+    var texto = mensagem;
+    var textoFinal = "";
+
+    for(var i = 0; i < texto.length; i++) {
+        if(texto[i] == "a"){
+            textoFinal = textoFinal + "a"
+            i = i + 1;
+        }
+
+         else if(texto[i] == "e"){
+            textoFinal = textoFinal + "e"
+            i = i + 4;
+        }
+
+        else if(texto[i] == "i"){
+            textoFinal = textoFinal + "i"
+            i = i + 3;
+        }
+
+        else if(texto[i] == "o"){
+            textoFinal = textoFinal + "o"
+            i = i + 3;
+        }
+
+        else if(texto[i] == "u"){
+            textoFinal = textoFinal + "u"
+            i = i + 3;
+        }
+
+        else {
+            textoFinal = textoFinal + texto[i];
+        }
+    }
+
+    return textoFinal;
+}
+
+function copiar () {
+    let texto =  document.getElementById(textoFinal)
+    navigator.clipboard.writeText(texto);
+  }
+  
